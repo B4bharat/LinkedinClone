e79a4e4a7c76099de3018aa8e23bf74268947fa1
Bharat Poptwani <bharat.poptwani@gmail.com>
2022-09-04 14:08:07 +0530

    HeaderOption component created

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
-import './Header.css';
-
 import SearchIcon from '@mui/icons-material/Search';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import HomeIcon from '@mui/icons-material/Home';
+import SupervisorAccountIcon from '@mui/icons-material/SupervisorAccount';
+
+import HeaderOption from './HeaderOption';
+
+import './Header.css';
 
 function Header() {
   return (
@@ -16,7 +20,10 @@ function Header() {
           <input type="text" />
         </div>
       </div>
-      <div className="header__right"></div>
+      <div className="header__right">
+        <HeaderOption Icon={HomeIcon} title="Home" />
+        <HeaderOption Icon={SupervisorAccountIcon}  title="My Network" />
+      </div>
     </div>
   )
 }
