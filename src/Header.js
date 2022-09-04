93b75ba895b3025fc70f6e37671fdf940e19bac2
Bharat Poptwani <bharat.poptwani@gmail.com>
2022-09-04 19:03:03 +0530

    header navigation bar styled

--- src/Header.js
+++ src/Header.js
@@ -4,6 +4,11 @@ import SearchIcon from '@mui/icons-material/Search';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import HomeIcon from '@mui/icons-material/Home';
 import SupervisorAccountIcon from '@mui/icons-material/SupervisorAccount';
+import BusinessCenterIcon from '@mui/icons-material/BusinessCenter';
+import ChatIcon from '@mui/icons-material/Chat';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+
+import Avatar from './assets/avatar-library/avatar-3.jpg';
 
 import HeaderOption from './HeaderOption';
 
@@ -23,6 +28,10 @@ function Header() {
       <div className="header__right">
         <HeaderOption Icon={HomeIcon} title="Home" />
         <HeaderOption Icon={SupervisorAccountIcon}  title="My Network" />
+        <HeaderOption Icon={BusinessCenterIcon} title="Jobs" />
+        <HeaderOption Icon={ChatIcon} title="Messaging" />
+        <HeaderOption Icon={NotificationsIcon} title="Notifications" />
+        <HeaderOption avatar={Avatar} title="me" />
       </div>
     </div>
   )
--- src/HeaderOption.js
+++ src/HeaderOption.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './HeaderOption.css';
 
-function HeaderOption({ Icon, title }) {
+import { Avatar } from '@mui/material';
+
+function HeaderOption({ avatar, Icon, title }) {
   return (
     <div className="headerOption">
       {Icon && <Icon className="headerOption__icon" />}
+      {avatar && <Avatar className="headerOption__icon" src={avatar} />}
       <h3 className="headerOption__title">{title}</h3>
     </div>
   )
